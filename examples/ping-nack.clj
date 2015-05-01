6eb297b09eefba6749b4bfb9502e725f567700c0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-05-01 20:04:57 +0300

    Refined and documented.

--- examples/ping-nack.clj
+++ examples/ping-nack.clj
@@ -1,44 +1,65 @@
 ;; Copyright (C) by Vesa Karvonen
 
 (require '[poc.cml :refer [gete pute choose wrap guard with-nack go-sync!]])
-(require '[clojure.core.async :refer [<! >! chan go put! timeout]])
+(require '[clojure.core.async :refer [<! <!! chan go put! timeout]])
 
-(def pong-ch (chan))
-(def ping-ch (chan))
+;; This is a very simple toy example that uses most of the features of CML-style
+;; composable events.  The idea is that we create a `ping` event that basically
+;; works like a timeout of 1000ms.  Internally the event uses an asynchronous
+;; server that takes ping requests.  It then waits for 1000ms or until the ping
+;; request's negative acknowledgment (nack) becomes available.  If 1000ms passes
+;; without negative acknowledgment, the ping server attempts to reply to the
+;; request or cancels in case the negative acknowledgment becomes available.
 
-(go
-  (loop []
-    (let [[nack ch m]
-           (<! (go-sync!
-                 (choose
-                   (wrap (gete pong-ch) (fn [[nack ch]] [nack ch "ping "]))
-                   (wrap (gete ping-ch) (fn [[nack ch]] [nack ch "pong "])))))]
-      (<! (timeout 1000))
-      (<! (go-sync!
-            (choose
-              (wrap nack (fn [_] (println "nack")))
-              (wrap (pute ch m) (fn [_] (println "put" m)))))))
-    (recur)))
+;; Obviously this is just a toy example, but this demonstrates the idea that
+;; a non-trivial asynchronous client-server protocol can be encapsulated as a
+;; composable event (in this case the `ping` event).  Plain core.async `alt!`
+;; does not support such composition.
 
-(go
-  (println
-    (<! (go-sync!
-          (choose
-            (with-nack
-              (fn [nack]
-                (let [reply-ch (chan)]
-                  (put! pong-ch [nack reply-ch])
-                  (gete reply-ch))))
-            (wrap (gete (timeout 1500))
-              (fn [_] "timeout1"))))))
+(def ping
+  (let [request-ch (chan)] ;; The internal ping request channel.
+    (go
+      ;; The internal asynchronous ping server loop.
+      (loop []
+        (let [;; Get a ping request.
+              [nack reply-ch]
+                (<! request-ch)
+              ;; Just wrap the nack event to print a diagnostic.
+              nack
+                (wrap nack (fn [_] (println "nack") true))
+              ;; Wait for 1000ms or nack.
+              already-nack
+                (<! (go-sync!
+                      (choose
+                        (wrap (timeout 1000) (fn [_] false))
+                        nack)))]
+          ;; If 1000ms passed without nack, try to reply or cancel via nack.
+          (when-not already-nack
+            (<! (go-sync!
+                  (choose
+                    nack
+                    (wrap (pute reply-ch "pong") (fn [_] (println "put"))))))))
+        (recur)))
 
-  (println
-    (<! (go-sync!
-          (choose
-            (with-nack
-              (fn [nack]
-                (let [reply-ch (chan)]
-                  (put! ping-ch [nack reply-ch])
-                  (gete reply-ch))))
-            (wrap (gete (timeout 500))
-              (fn [_] "timeout2")))))))
+    ;; The client side event definition.
+    (with-nack
+      (fn [nack]
+        (let [reply-ch (chan)]
+          ;; The nack and the reply channel are sent to the server.
+          (put! request-ch [nack reply-ch])
+          ;; Then we return an event that synchronizes on the reply.
+          reply-ch)))))
+
+;; First we try to get a ping within 500ms.
+(println
+  (<!! (go-sync!
+         (choose
+           ping
+           (wrap (timeout 500) (fn [_] "timeout 500"))))))
+
+;; Then we try to get a ping within 1500ms.
+(println
+  (<!! (go-sync!
+         (choose
+           ping
+           (wrap (timeout 1500) (fn [_] "timeout 1500"))))))
