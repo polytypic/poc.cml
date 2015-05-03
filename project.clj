6bdeb2f9f90c139c8a5985bcb6e24e87d84089e8
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-05-03 19:00:49 +0300

    Update.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject poc.cml "0.1.0-SNAPSHOT"
+(defproject poc.cml "0.1.0-SNAPSHOT2"
   :description "Proof-of-Concept CML-style composable first-class events on top of core.async."
   :url "https://github.com/VesaKarvonen/poc.cml"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.5"]]
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3126"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :cljsbuild {:builds [{:source-paths ["src"]}]})
